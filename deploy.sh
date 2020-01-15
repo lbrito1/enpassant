ea1cf4362f5dbf94dfb65e900c77e6324298c95c
Leonardo Brito <lbrito@gmail.com>
2020-01-14 21:25:36 -0300

    Updates deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
 # Crawls through their site, creates/updates JSON store
-echo "Crawling site..."
+echo ">>>> Crawling site..."
 ruby -e 'require "./compiler/app/crawler"; Crawler.new.call'
 
 # Regenerates the static site
 cd viewer
-echo "Switching to development branch..."
+echo ">>>> Switching to development branch..."
 git checkout development
 
-echo "Calling nanoc..."
+echo ">>>> Calling nanoc..."
 nanoc
 
-echo "Adding files with git add..."
+echo ">>>> Adding files with git add..."
 git add .
 
-echo "Commiting..."
+echo ">>>> Commiting..."
 git commit -m "Updates static files (automated)."
 
-echo "Pushing to development branch..."
+echo ">>>> Pushing to development branch..."
 git push origin development
 
 # Copies the static site files to the correct branch
-echo "Switching to master (github.io) branch..."
+echo ">>>> Switching to master (github.io) branch..."
 git checkout master
 
-echo "Copying output files from development branch..."
+echo ">>>> Copying output files from development branch..."
 git checkout development output/
 
-echo "Moving files to root dir..."
+echo ">>>> Moving files to root dir..."
 mv output/* ./
 
-echo "Cleaning up output folder..."
+echo ">>>> Cleaning up output folder..."
 git reset HEAD output/ && git clean -df output/
 
 # Pushes the changes to the Github Pages branch
-echo "Adding update site files with git add..."
+echo ">>>> Adding update site files with git add..."
 git add .
 
-echo "Committing..."
+echo ">>>> Committing..."
 git commit -m "Updates static files (automated)."
 
-echo "Pushing to master (github.io) branch..."
+echo ">>>> Pushing to master (github.io) branch..."
 git push origin master
 
-echo "Checking out development branch..."
+echo ">>>> Checking out development branch..."
 git checkout development
 
-echo "Done!"
+echo ">>>> Done!"
