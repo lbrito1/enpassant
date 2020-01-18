feda26037b6f73d082c2e8f3208ba0a4966dfdea
Leonardo Brito <lbrito@gmail.com>
2020-01-17 22:13:02 -0300

    Improves layout.

--- viewer/lib/helpers/recommendation.rb
+++ viewer/lib/helpers/recommendation.rb
@@ -40,7 +40,7 @@ class Recommendation
 
   # TODO move to presenter
   def icon_name
-    norm = Category::normalized_category(category)
+    norm = Category::normalize(category)
     return 'film' if norm == 'filme'
     return 'tv' if norm == 'série'
     return 'music' if norm == 'música'
